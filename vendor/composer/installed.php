ddc44e7719812a5067e14e71e4cc18b39aa5f1ea
Whiletruend <whiletruend@gmail.com>
2025-04-05 22:22:01 +0200

    Ajout de composer (fichiers à modifier pour faire correspondre à votre PC, on verra ça ensemble)

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'benja/e-learning-project-m1sic',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ac1cd5312a31b897faf823484e79083b289782b7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'benja/e-learning-project-m1sic' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ac1cd5312a31b897faf823484e79083b289782b7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
