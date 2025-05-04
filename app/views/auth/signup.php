326a5615515e71108a7b883240fe4458b7ef4119
Whiletruend <whiletruend@gmail.com>
2025-05-04 13:23:45 +0000

    Correction pb d'enregistrement

--- app/views/auth/signup.php
+++ app/views/auth/signup.php
@@ -81,7 +81,7 @@
         <div class="logo-learn2master">📘 Learn2Master</div>
         <h2 class="fw-bold mb-4">Créer un compte</h2>
 
-        <form method="post" action="/signUp" onsubmit="return validatePasswords()">
+        <form method="post" action="/signup" onsubmit="return validatePasswords()">
           <div class="form-outline mb-4">
             <input type="text" id="username" name="username" placeholder="Nom d'utilisateur" class="form-control" required />
           </div>
